23db5c53ba8420e3a137f1d64ebcc4e64216a913
wzh <972036796@qq.com>
2019-07-17 17:29:32 +0800

    阅卷管理页面逻辑完善

--- manage/src/services/marking.js
+++ manage/src/services/marking.js
@@ -3,3 +3,8 @@ export function get_Awaiting_approval_class(){
     //获取已经分配教室的班级
     return request.get("/manger/grade")
 }
+export function get_exam_student(params){
+    console.log(params)
+    //获取学生试卷列表接口
+    return request.get("/exam/student",{params})
+}
